e287bca2eb8c4cc0868f04b8ccc639d550cf3f7c
Bunmeng Bo <bunmeng.bo@gmail.com>
2021-03-08 14:50:43 +0100

    my first commit

---
+++ Shared/my_scheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_scheduleApp.swift
+//  Shared
+//
+//  Created by Bunmeng Bo on 08.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct my_scheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
